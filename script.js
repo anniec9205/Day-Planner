2e614a7abad38763b7f18f79040fa4da51aeaf0b
anniec9205 <danbi9205@gmail.com>
2021-03-30 22:41:49 +1100

    added time for html and 11am for js

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ var presentTime = moment();
 presentTime = presentTime.startOf("hour");
 // Calculates the start of day to 9am (by adding 9)
 var beforeTime = moment().startOf('day').add(9, "hours");
+
+// Time blocks
+// 9 AM 
+var time1 = beforeTime.add(0, "h");
+// var time1 = beforeTime;
+time1 = time1.format('hh:mm A');
+// puts time formula into html
+$(".block1").text(time1);
+// 10 AM
+var time2 = beforeTime.add(1, "h");
+time2 = time2.format('hh:mm A');
+// Populates time formula into html
+$(".block2").text(time2);
+// 11 AM
+var time2 = beforeTime.add(2, "h");
+time2 = time2.format('hh:mm A');
+// Populates time formula into html
+$(".block3").text(time2);
