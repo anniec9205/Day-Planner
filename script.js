fc21bcc16aa612e80496a2e03e921d995cdea047
anniec9205 <danbi9205@gmail.com>
2021-03-30 22:16:00 +1100

    timeblocks added on html and for js for 9am and 10am

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //Declaration for day and time
 moment(Date);
-$("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
-// Moment Current time 
-var currentTime = moment();
-// Returns current time to the nearest hour - 12:30 becomes 12:00
-currentTime = currentTime.startOf("hour");
-// Calculates the start of day + 9 to return 9 am
+$("#presentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
+// Moment Present time 
+var presentTime = moment();
+// Returns current time to the nearest hour - rounded down
+presentTime = presentTime.startOf("hour");
+// Calculates the start of day to 9am (by adding 9)
 var beforeTime = moment().startOf('day').add(9, "hours");
