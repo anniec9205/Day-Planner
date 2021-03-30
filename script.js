c2f1ee6d673f38949efb42388c7e47a816818bdb
anniec9205 <danbi9205@gmail.com>
2021-03-30 22:43:33 +1100

    added the timeslots

--- script.js
+++ script.js
@@ -21,7 +21,37 @@ time2 = time2.format('hh:mm A');
 // Populates time formula into html
 $(".block2").text(time2);
 // 11 AM
-var time2 = beforeTime.add(2, "h");
+var time2 = beforeTime.add(1, "h");
 time2 = time2.format('hh:mm A');
 // Populates time formula into html
 $(".block3").text(time2);
+// 12 PM
+var time4 = beforeTime.add(1, "h");
+time4 = time4.format('hh:mm A');
+// Populates time formula into html
+$(".block4").text(time4);
+// 1 PM
+var time5 = beforeTime.add(1, "h");
+time5 = time5.format('hh:mm A');
+// Populates time formula into html
+$(".block5").text(time5);
+// 2 PM
+var time6 = beforeTime.add(1, "h");
+time6 = time6.format('hh:mm A');
+// Populates time formula into html
+$(".block6").text(time6);
+// 3 PM
+var time7 = beforeTime.add(1, "h");
+time7 = time7.format('hh:mm A');
+// Populates time formula into html
+$(".block7").text(time7);
+// 4 PM
+var time8 = beforeTime.add(1, "h");
+time8 = time8.format('hh:mm A');
+// Populates time formula into html
+$(".block8").text(time8);
+// 5 PM
+var time9 = beforeTime.add(1, "h");
+time9 = time9.format('hh:mm A');
+// Populates time formula into html
+$(".block9").text(time9);
